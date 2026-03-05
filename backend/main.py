6ebb84d43b6f86aa9704baf37ba66466aedee493
Neo <neonpulse142k@gmail.com>
2026-03-05 16:51:12 +0530

    ml model done

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from backend!")
+import requests
 
+url = "https://abc123.ngrok-free.app/generate"
+
+data = {
+    "text": "Explain Redis caching"
+}
+
+res = requests.post(url, json=data)
+
+print(res.json())
 
 if __name__ == "__main__":
     main()
